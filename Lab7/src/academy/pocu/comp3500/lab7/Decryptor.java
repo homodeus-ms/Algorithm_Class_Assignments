e96702a4d02c5b16ee5601e77585e25a440648dc
X1_LAPTOP\seekc <seekchange@daum.net>
2024-02-27 16:07:35 +0900

    실습7 시간복잡도

--- Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
+++ Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Decryptor {
     private final String[] codewords;
-    private int[] strLengths;
+    private final int[] strLengths;
 
     public Decryptor(final String[] codewords) {
         int length = codewords.length;
@@ -25,14 +25,33 @@ public class Decryptor {
             return new String[]{};
         }
 
-        int[] counts = new int[26];
-        int[] keep = new int[26];
-        ArrayList<String> result = new ArrayList<>(codewords.length);
-
         int wordLength = word.length();
+
+        int[] wordToLowerCase = new int[wordLength];
+        int[] wordCharCount = new int[wordLength];
+        //int wordToLowerCaseLength = 0;
+
+        int[] counts = new int[26];
+        //int[] keep = new int[26];
+        ArrayList<String> result = new ArrayList<>(codewords.length);
         int specialCharCount = 0;
 
         for (int i = 0; i < wordLength; ++i) {
+            int c = (word.charAt(i) | 0x20) - 'a';
+            // ('?' | 0x20) - 'a' = -34
+            if (c == -34) {
+                ++specialCharCount;
+                wordToLowerCase[i] = -1;
+                wordCharCount[i] = -1;
+                continue;
+            }
+            wordToLowerCase[i] = c;
+            ++wordCharCount[i];
+            ++counts[c];
+            //++wordToLowerCaseLength;
+        }
+
+        /*for (int i = 0; i < wordLength; ++i) {
             char c = word.charAt(i);
             if (c == '?') {
                 ++specialCharCount;
@@ -42,7 +61,7 @@ public class Decryptor {
                 ++counts[c];
                 ++keep[c];
             }
-        }
+        }*/
 
 
         int keepSpecialCharCount = specialCharCount;
@@ -61,16 +80,14 @@ public class Decryptor {
             found = true;
 
             for (int j = 0; j < strLength; ++j) {
-                char c = str.charAt(j);
-                c |= 0x20;
-                c -= 'a';
+                int c = str.charAt(j) - 'a';
                 if (counts[c] != 0) {
                     --counts[c];
                 } else if (specialCharCount != 0) {
                     --specialCharCount;
                 } else {
                     found = false;
-                    break;
+                    //break;
                 }
             }
 
@@ -78,16 +95,23 @@ public class Decryptor {
                 result.add(str);
             }
 
+            for (int j = 0; j < wordToLowerCase.length; ++j) {
+                int idx = wordToLowerCase[j];
+                if (idx != -1) {
+                    counts[idx] += wordCharCount[j];
+                }
+            }
+
             specialCharCount = keepSpecialCharCount;
-            copyArr(counts, keep);
+            //copyArr(counts, keep);
         }
 
         return result.toArray(new String[0]);
     }
-    private void copyArr(int[] dst, int[] src) {
+    /*private void copyArr(int[] dst, int[] src) {
         assert (dst.length == src.length);
         for (int i = 0; i < dst.length; ++i) {
             dst[i] = src[i];
         }
-    }
+    }*/
 }
